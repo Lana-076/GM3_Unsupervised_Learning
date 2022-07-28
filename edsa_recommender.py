3861bc23956bab359d1aa53faa4c7002592ea6a3
Lana-076 <katleholebona@gmail.com>
2022-07-28 17:05:41 +0200

    Streamlit and extras

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -27,10 +27,12 @@
 """
 # Streamlit dependencies
 import streamlit as st
+from PIL import Image
 
 # Data handling dependencies
 import pandas as pd
 import numpy as np
+import base64
 
 # Custom Libraries
 from utils.data_loader import load_movie_titles
@@ -40,18 +42,22 @@ from recommenders.content_based import content_model
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
 
+
 # App declaration
 def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Recommender System","Exploratory Data Analysis","Solution Overview", "About/Contact Us"]
+    
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
     # -------------------------------------------------------------------
     page_selection = st.sidebar.selectbox("Choose Option", page_options)
     if page_selection == "Recommender System":
+        logo = Image.open('resources/imgs/team/logo.png')
+        st.sidebar.image(logo, use_column_width=True)
         # Header contents
         st.write('# Movie Recommender Engine')
         st.write('### EXPLORE Data Science Academy Unsupervised Predict')
@@ -101,12 +107,158 @@ def main():
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
     if page_selection == "Solution Overview":
+        logo = Image.open('resources/imgs/team/logo.png')
+        st.sidebar.image(logo, use_column_width=True)
+        # Background of the app
+        st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url("https://github.com/Orion316/mypackage/blob/master/SolutionOverview.jpeg?raw=true");
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
         st.title("Solution Overview")
-        st.write("Describe your winning approach on this page")
+        st.markdown("![Alt Text](https://cdn.dribbble.com/users/31818/screenshots/1891002/math.gif)")
+
+        st.write("There were two sections to this predict and both sections had content based and collaborative filtering. The content based filtering showed good signs of predicting a rating but needed way more computing power to make predict ratings on massive dataset. Therefore was hard to evaluate if the predictor was accurate")
+        st.subheader("What can be done differently next time")
+
+        st.write("The use of metrics such as hit rate, coverage and diversity would be taken into account, this will help evaluating how well the recommendation systems perform. Adjusting the threshold of the long-tail distribution so that movies that are not popular but fit users criteria could be recommended instead of just popular movies.")
 
     # You may want to add more sections here for aspects such as an EDA,
     # or to provide your business pitch.
 
+    # ------------- EDA Section of the App -------------------
+    if page_selection == "Exploratory Data Analysis":
+        logo = Image.open('resources/imgs/team/logo.png')
+        st.sidebar.image(logo, use_column_width=True)
+        # Background of the app
+        st.markdown(
+                f"""
+                <style>
+                .stApp {{
+                    background: url("https://github.com/Orion316/mypackage/blob/master/eda.png?raw=true");
+                    background-size: cover
+                }}
+                </style>
+                """,
+                unsafe_allow_html=True
+            )
+
+        st.title("Exploratory Data Analysis")
+        st.write("Exploratory data analysis is an approach to analyzing data sets to summarize their main characteristics, often with visual methods. EDA is the critical process of performing initial investigations on data to discover patterns, to spot anomalies, to test hypothesis and to check assumptions with the help of summary statistics and graphical representations. A statistical model can be used or not, but primarily EDA is for seeing what the data can tell us beyond the formal modelling or hypothesis testing task. It is a good practice to understand the data first and try to gather as many insights from it. EDA is all about making sense of data in hand, before getting them dirty with it, which will be done below. Pandas dataframe.info() function is used to get a concise summary of the dataframe. It comes really handy when doing exploratory analysis of the data")
+        st.title("Count of Ratings")
+        st.image('resources/imgs/EDA/1.png',use_column_width=True)
+        st.title("Movies Duration Bar Graph")
+        st.image('resources/imgs/EDA/2.png',use_column_width=True)
+        st.title("Density Plot for the Ratings")
+        st.image('resources/imgs/EDA/3.png',use_column_width=True)
+        st.title("Yearly Analysis of Movies")
+        st.image('resources/imgs/EDA/4.png',use_column_width=True)
+        
+
+
+    # ------------- Model Section of the App -------------------
+    if page_selection == "About/Contact Us":
+        logo = Image.open('resources/imgs/team/logo.png')
+        st.sidebar.image(logo, use_column_width=True)
+
+        st.title("About/Contact Us")
+	    #st.markdown("Solid Solutions is an innovation tech company with a key focus on creating up to date technological products designed to make light of any problem thrown our way. We are extremely passionate about giving back to the community. Strengthening Today for a Stronger Tomorrow!")
+		# You can read a markdown file from supporting resources folder
+
+        col1, col2, col3, col4 = st.columns(4)
+        img1 = Image.open("resources/imgs/team/Katlego.jpeg")
+        img2 = Image.open("resources/imgs/team/Aphiwe.jpeg")
+        img3 = Image.open("resources/imgs/team/Nomfundo.jpeg")
+        img4 = Image.open("resources/imgs/team/me.jpeg")
+
+        with col1: 
+            st.caption("CEO")
+            st.image(img1)
+            st.caption("Tholwana Katleho Lebona")
+
+        with col2: 
+            st.caption("Market Technologist")
+            st.image(img2)
+            st.caption("Aphiwe Rasisemula")
+
+        with col3: 
+            st.caption("UI/UX Designer")
+            st.image(img3)
+            st.caption("Nomfundo Mbambo")
+
+        with col4: 
+            st.caption("Full-stack Developer")
+            st.image(img4)
+            st.caption("Morema Moloisi")
+
+
+        st.subheader("Message Us")
+        with st.form("form", clear_on_submit=True):
+            name = st.text_input("Enter Full Name")
+            email = st.text_input("Enter Email Address")
+            message = st.text_area("Message")
+
+            submit = st.form_submit_button("Submit")
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.subheader("Address")
+            img = Image.open("resources/imgs/team/map.png")
+            st.image(img)
+            st.markdown("137 Saisy")
+            st.markdown("Johannesburg, Sandton")
+            st.markdown("2196")
+
+        with col2:
+            st.subheader("Phone")
+            st.markdown("Monday - Friday")
+            st.markdown("08h00 - 17h00 GMT+2")
+            st.markdown("(+27) 021 554 1091")
+            st.markdown("(+27) 081 579 4965")
+
+        with col3:
+            st.subheader("Email")
+            st.markdown("katleho@datagernalist.com")
+            st.markdown("morema@datagernalist.com")
+
+
+        st.markdown(
+                f"""
+                <style>
+                .stApp {{
+                    background: url("https://github.com/Orion316/mypackage/blob/master/work.jpeg?raw=true");
+                    background-size: cover
+                }}
+                </style>
+                """,
+                unsafe_allow_html=True
+            )
+        
+
+
+
+    
+
+
+    
+# Background of the app
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url("https://github.com/Orion316/mypackage/blob/master/Background.png?raw=true");
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 
 if __name__ == '__main__':
     main()
--- recommenders/collaborative_based.py
+++ recommenders/collaborative_based.py
@@ -12,14 +12,6 @@
 
     NB: You are required to extend this baseline algorithm to enable more
     efficient and accurate computation of recommendations.
-
-    !! You must not change the name and signature (arguments) of the
-    prediction function, `collab_model` !!
-
-    You must however change its contents (i.e. add your own collaborative
-    filtering algorithm), as well as altering/adding any other functions
-    as part of your improvement.
-
     ---------------------------------------------------------------------
 
     Description: Provided within this file is a baseline collaborative
@@ -27,42 +19,77 @@
 
 """
 
-# Script dependencies
+
 import pandas as pd
 import numpy as np
 import pickle
 import copy
+import os
+import scipy as sp
+import operator
 from surprise import Reader, Dataset
 from surprise import SVD, NormalPredictor, BaselineOnly, KNNBasic, NMF
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+from surprise.model_selection import cross_validate
 
-# Importing data
-movies_df = pd.read_csv('resources/data/movies.csv',sep = ',')
-ratings_df = pd.read_csv('resources/data/ratings.csv')
-ratings_df.drop(['timestamp'], axis=1,inplace=True)
 
-# We make use of an SVD model trained on a subset of the MovieLens 10k dataset.
-model=pickle.load(open('resources/models/SVD.pkl', 'rb'))
+# Importing train and test datasets
+ratings_df = pd.read_csv('resources/data/ratings.csv').drop(columns=['timestamp'],axis=0)
+movies_df = pd.read_csv('resources/data/movies.csv')
+
+def load_movie_titles(path_to_movies):
+    """Load movie titles from database records.
+    Parameters
+    ----------
+    path_to_movies : str
+        Relative or absolute path to movie database stored
+        in .csv format.
+    Returns
+    -------
+    list[str]
+        Unordered list of movie titles.
+    """
+    df = pd.read_csv(path_to_movies)
+    df = df.dropna()
+    movie_list = df['title'].to_list()
+    return movie_list
+
+title_list = load_movie_titles('resources/data/movies.csv')
+movies1 = title_list[14930:15200]
+movies2 = title_list[25055:25255]
+movies3 = title_list[21100:21200]
+Moviez = movies1 + movies2 + movies3
+edsa_movies = movies_df[movies_df['title'].isin(Moviez)]
+
+edsa_movies[300:350]
+
+def create_subset(movies_sample,all_movies,all_ratings):
+    ratings_ids = all_ratings[all_ratings['movieId'].isin(movies_sample['movieId'].tolist())]
+    ratings_subset = all_ratings[all_ratings['userId'].isin(ratings_ids['userId'].tolist())]
+    return ratings_subset
+
+ratings_subset = create_subset(edsa_movies,movies_df,ratings_df)
+
+ratings_subset = ratings_subset[:1000000]
+
+model=pickle.load(open('resources/models/model', 'rb'))
 
 def prediction_item(item_id):
     """Map a given favourite movie to users within the
        MovieLens dataset with the same preference.
-
     Parameters
     ----------
     item_id : int
         A MovieLens Movie ID.
-
     Returns
     -------
     list
         User IDs of users with similar high ratings for the given movie.
-
     """
     # Data preprosessing
     reader = Reader(rating_scale=(0, 5))
-    load_df = Dataset.load_from_df(ratings_df,reader)
+    load_df = Dataset.load_from_df(ratings_subset,reader)
     a_train = load_df.build_full_trainset()
 
     predictions = []
@@ -73,76 +100,110 @@ def prediction_item(item_id):
 def pred_movies(movie_list):
     """Maps the given favourite movies selected within the app to corresponding
     users within the MovieLens dataset.
-
     Parameters
     ----------
     movie_list : list
         Three favourite movies selected by the app user.
-
     Returns
     -------
     list
         User-ID's of users with similar high ratings for each movie.
-
     """
     # Store the id of users
     id_store=[]
+    movie_ids = []
+    for movie in movie_list:
+        movie_ids.append(int(edsa_movies['movieId'][edsa_movies['title']==movie]))
     # For each movie selected by a user of the app,
     # predict a corresponding user within the dataset with the highest rating
-    for i in movie_list:
+    for i in movie_ids:
         predictions = prediction_item(item_id = i)
         predictions.sort(key=lambda x: x.est, reverse=True)
         # Take the top 10 user id's from each movie with highest rankings
-        for pred in predictions[:10]:
+        for pred in predictions[:100]:
             id_store.append(pred.uid)
     # Return a list of user id's
     return id_store
 
-# !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
-# You are, however, encouraged to change its content.  
-def collab_model(movie_list,top_n=10):
-    """Performs Collaborative filtering based upon a list of movies supplied
-       by the app user.
-
+def collab_model(movie_list,top_n):
+    """Short summary.
     Parameters
     ----------
-    movie_list : list (str)
-        Favorite movies chosen by the app user.
+    movie_list : type
+        Description of parameter `movie_list`.
     top_n : type
-        Number of top recommendations to return to the user.
-
+        Description of parameter `top_n`.
     Returns
     -------
-    list (str)
-        Titles of the top-n movie recommendations to the user.
-
+    type
+        Description of returned object.
     """
+    # get movie ids for movie_list
+    movie_ids = []
+    for movie in movie_list:
+        movie_ids.append(int(edsa_movies['movieId'][edsa_movies['title']==movie]))
 
-    indices = pd.Series(movies_df['title'])
-    movie_ids = pred_movies(movie_list)
-    df_init_users = ratings_df[ratings_df['userId']==movie_ids[0]]
-    for i in movie_ids :
-        df_init_users=df_init_users.append(ratings_df[ratings_df['userId']==i])
-    # Getting the cosine similarity matrix
-    cosine_sim = cosine_similarity(np.array(df_init_users), np.array(df_init_users))
-    idx_1 = indices[indices == movie_list[0]].index[0]
-    idx_2 = indices[indices == movie_list[1]].index[0]
-    idx_3 = indices[indices == movie_list[2]].index[0]
-    # Creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-     # Appending the names of movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
-    recommended_movies = []
-    # Choose top 50
-    top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:top_n]:
-        recommended_movies.append(list(movies_df['title'])[i])
-    return recommended_movies
+    # Create list of users which would rate these movies highly
+    user_ids = pred_movies(movie_list)
+    
+    # Create dataframe of all the movies that these users have rated
+    df_init_users = ratings_subset[ratings_subset['userId'].isin(user_ids)]
+    
+    # Add new user with ratings to userlist
+    new_row1 = {'userId':200000, 'movieId':movie_ids[0], 'rating':5.0}
+    new_row2 = {'userId':200000, 'movieId':movie_ids[1], 'rating':5.0}
+    new_row3 = {'userId':200000, 'movieId':movie_ids[2], 'rating':4.5}
+    df_init_users = df_init_users.append([new_row1,new_row2,new_row3], ignore_index=True)
+    
+    # Creating Util matrix,replace NANs and transpose
+    util_matrix = pd.pivot_table(df_init_users,values='rating',columns='movieId',index='userId')
+    util_matrix_norm = util_matrix.apply(lambda x: (x-np.mean(x))/(np.max(x)-np.min(x)), axis=1)
+    util_matrix_norm.fillna(0, inplace=True)
+    util_matrix_norm = util_matrix_norm.T
+    util_matrix_norm = util_matrix_norm.loc[:, (util_matrix_norm != 0).any(axis=0)]
+    
+    # Save the utility matrix in scipy's sparse matrix format
+    util_matrix_sparse = sp.sparse.csr_matrix(util_matrix_norm.values)
+    
+    # Compute the similarity matrix using the cosine similarity metric
+    user_similarity = cosine_similarity(util_matrix_sparse.T)
+    
+    # Save the matrix as a dataframe to allow for easier indexing  
+    user_sim_df = pd.DataFrame(user_similarity,index = util_matrix_norm.columns,columns = util_matrix_norm.columns)
+    
+    user = 200000
+    p = 20
+    # Gather the p users which are most similar to the reference user 
+    sim_users = user_sim_df.sort_values(by=user, ascending=False).index[1:p+1]
+    favorite_user_items = [] 
+    common_favorites = {} 
+
+    for i in sim_users:
+        # Maximum rating given by the current user to an item 
+        max_score = util_matrix_norm.loc[:, i].max()
+        # Save the names of items maximally rated by the current user   
+        favorite_user_items.append(util_matrix_norm[util_matrix_norm.loc[:, i]==max_score].index.tolist())
+
+    # Loop over each user's favorite items and tally which ones are 
+    # most popular overall.
+    for item_collection in range(len(favorite_user_items)):
+        for item in favorite_user_items[item_collection]:
+            if item in common_favorites:
+                common_favorites[item] += 1
+            else:
+                common_favorites[item] = 1
+
+    # Sort the overall most popular items and return the top-N instances
+    sorted_list = sorted(common_favorites.items(), key=operator.itemgetter(1), reverse=True)[:top_n]
+    top_N = [x[0] for x in sorted_list]
+    
+    # Return Movie Names
+    recommendations = []
+    for movieid in top_N:
+        recommendations.append(movies_df[movies_df['movieId']==movieid]['title'].tolist())
+    recommendations = [item for sublist in recommendations for item in sublist]
+    recommendations = [x for x in recommendations if x not in movie_list]
+    recommendations[:top_n]
+
+    return recommendations
+
